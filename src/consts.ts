72e0ee15c19649b6a6c1b21753aba315befcbdd3
peksami <peksami@gmail.com>
2025-09-21 19:58:43 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Norgelan.com";
+export const SITE_TITLE = "Finn det beste lånet for deg - raskt og trygt";
 export const SITE_DESCRIPTION = "Sammenlign lån og finn det beste lånet for dine behov.";
